c9ccdc295821373df5f239f160182da478307cc6
simon moro <simon.moro@imt-atlantique.net>
2021-11-17 12:13:17 +0100

    Adding .db support instead of json + http requests with params

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,21 @@ import * as userController from "./userController"
 import { User } from './model'
 
 export const register = ( app: express.Application ) => {
-  app.get('/', (req, res) => res.send('Hello World !'));
+  app.get('/', (req, res) => res.send('Hello World (local repo)!'));
 
   app.get('/user', (req, res) => {
 	res.status(200).json(userController.listUsers())
   })
+
+  app.get('/user/:name', (req, res) => {
+    var params=req.params.name
+    var user:User = userController.getUser(params)
+    var name:string = user.name
+    var score:number = user.score
+    res.status(200).send(name + " " + score.toString())
+    })
+  
+
  
   app.post('/user', (req, res) => {
 	const newUser: User = req.body    
